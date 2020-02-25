3418965630f47d188b8e4ad579a784444dd5a21d
Chaiaypong <mickey_mix@live.com>
2020-02-25 11:15:28 +0700

    init repo

---
+++ testdevops/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  testdevops
+//
+//  Created by chaiyapong on 2/25/20.
+//  Copyright © 2020 chaiyapong. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
